270103d1c07e3718f368d0ea2e9f6b0e13e72f02
MohamedHamdy1987 <shahat10101048@gmail.com>
2026-05-11 16:48:39 +0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import { ThemeProvider } from './contexts/ThemeContext'
+import { AppProvider } from './contexts/AppContext'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <ThemeProvider>
+        <AppProvider>
+          <App />
+        </AppProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+)
+
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+  })
+}
